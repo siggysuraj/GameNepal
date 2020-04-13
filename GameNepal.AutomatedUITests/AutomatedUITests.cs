5f65626232fa7b87436b3580e84ff9b2691dfffa
siggysuraj <36285959+siggysuraj@users.noreply.github.com>
2020-04-12 19:57:15 -0500

    edit test class

--- GameNepal.AutomatedUITests/AutomatedUITests.cs
+++ GameNepal.AutomatedUITests/AutomatedUITests.cs
@@ -9,10 +9,14 @@ namespace GameNepal.AutomatedUITests
     public class AutomatedUITests:IDisposable
     {
         private readonly IWebDriver _driver;
+
+
         public AutomatedUITests()
         {
             _driver = new ChromeDriver(Environment.CurrentDirectory);
         }
+
+        
 
         public void Dispose()
         {
@@ -31,12 +35,32 @@ namespace GameNepal.AutomatedUITests
         [Fact]
         public void Register_test()
         {
+
             _driver.Navigate().GoToUrl("http://localhost:49550/Home/Register");
             WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
-            _driver.FindElement(By.Id("FirstName"))
-                .SendKeys("TestFirst");
-            _driver.FindElement(By.Id("LastName"))
-               .SendKeys("TestName");
+            /*   wait.Until<IWebElement>((d) =>
+               {
+                   IWebElement element = d.FindElement(By.Id("FirstName"));
+                   if (element.Selected)
+
+                   {
+                   element.SendKeys("suraj");
+                   }
+
+                   return null;
+               }); */
+
+            //   var wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(30));
+
+
+            IWebElement query = _driver.FindElement(By.Id("FirstName"));
+            query  .SendKeys("TestFirst");
+
+            // var element = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("FirstName"))
+            //  .SendKeys("TestFirst");
+            query = _driver.FindElement(By.Id("LastName"));
+             query  .SendKeys("TestName1111");
+
             _driver.FindElement(By.Id("Email"))
                .SendKeys("test113@abc.com");
             _driver.FindElement(By.Id("Phone"))
@@ -52,6 +76,7 @@ namespace GameNepal.AutomatedUITests
                .SendKeys("Test1234@");
             _driver.FindElement(By.Id("btnSend"))
                 .Click();
+            var wait1 = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
             Assert.Equal("Home", _driver.Title);
         }
     }
